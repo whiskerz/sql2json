3c25f2f2e862002ad303d22b35c5e29bbd3c8aec
whiskerz <coding@jbdb.de>
2017-02-12 19:12:44 +0100

    Added a test object mother to make sure test data is being created correctly

---
+++ src/test/java/de/jbdb/Sql2JSONTestObjects.java
@@ -0,0 +1,14 @@
+package de.jbdb;
+
+import net.sf.jsqlparser.expression.StringValue;
+
+public interface Sql2JSONTestObjects {
+
+	/**
+	 * STRING_VALUE needs to be a String beginning and ending with "'" since the jsqlparser lib expects Strings to use
+	 * this SQL style
+	 */
+	StringValue STRING_VALUE_01 = new StringValue("'testValue01'");
+	StringValue STRING_VALUE_02 = new StringValue("'testValue02'");
+
+}
