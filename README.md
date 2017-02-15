421827aaa137f7eb6f0997f9e35dd038b792099e
whiskerz <coding@jbdb.de>
2017-02-15 23:17:09 +0100

    Added references to README

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # sql2json
 Just a small project to convert sql dumps (basically long files with INSERT statements) to a json structure. 
+
+# Links and References
+
+https://github.com/junit-team/junit4/wiki/Rules
+https://www.w3schools.com/js/js_json_arrays.asp
+http://docs.oracle.com/javaee/7/tutorial/jsonp003.htm#BABHAHIA
+http://jsqlparser.sourceforge.net/example.php
+https://gist.github.com/spuklo
+http://moreunit.sourceforge.net/#doc-classtestswitching
+http://howtodoinjava.com/java-8/read-file-line-by-line-in-java-8-streams-of-lines-example/
+https://www.codementor.io/eh3rrera/using-java-8-method-reference-du10866vx
--- src/main/java/de/jbdb/sql2json/SqlDumpFileScanner.java
+++ src/main/java/de/jbdb/sql2json/SqlDumpFileScanner.java
@@ -3,18 +3,14 @@ package de.jbdb.sql2json;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
-import java.util.function.Consumer;
 import java.util.stream.Stream;
 
 public class SqlDumpFileScanner {
-	public enum State {
+	private enum State {
 		NO_INSERT, INSERT
-
 	}
 
 	private static final String INSERT_START = "INSERT";
@@ -22,32 +18,25 @@ public class SqlDumpFileScanner {
 	private static final String INSERT_END = ";";
 
 	private State state = State.NO_INSERT;
-	private ArrayList<String> insertList = new ArrayList<String>();
 
 	private StringBuffer currentInsert;
 
 	private HashMap<String, Insert> insertMap;
 
 	public Collection<Insert> scanDirectory(String[] directoryPath) {
-		Consumer<String> scanFile = (String file) -> scanFile(file);
-
-		Arrays.stream(directoryPath).forEach(scanFile);
+		Arrays.stream(directoryPath).forEach(this::scanFile);
 
 		return insertMap.values();
 	}
 
-	public List<String> scanFile(String filePath) {
-		Consumer<String> scanLine = (String line) -> scanLine(line);
-
+	public void scanFile(String filePath) {
 		try (Stream<String> stream = Files.lines(Paths.get(filePath))) {
 
-			stream.forEach(scanLine);
+			stream.forEach(this::scanLine);
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
-		return insertList;
 	}
 
 	private void scanLine(String line) {
