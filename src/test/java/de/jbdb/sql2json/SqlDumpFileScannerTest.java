a36b78bb83adf258a928f7b01131170bbef288b8
whiskerz <coding@jbdb.de>
2017-02-17 10:42:49 +0100

    Adding test for file scanner.

---
+++ src/test/java/de/jbdb/sql2json/SqlDumpFileScannerTest.java
@@ -0,0 +1,12 @@
+package de.jbdb.sql2json;
+
+import org.junit.Test;
+
+public class SqlDumpFileScannerTest {
+
+	@Test
+	public void testScanDirectory() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+}
