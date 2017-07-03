022b2235b0a730dbe4f38dacdebd760355761bb5
whiskerz <coding@jbdb.de>
2017-07-03 16:24:49 +0200

    Didn't work because ComponentScan only scans subdirectories unless a parent package is given.

--- src/main/java/de/jbdb/sql2json/cli/Sql2JSONCommandLine.java
+++ src/main/java/de/jbdb/sql2json/cli/Sql2JSONCommandLine.java
@@ -3,7 +3,9 @@ package de.jbdb.sql2json.cli;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("de.jbdb.sql2json")
 @SpringBootApplication
 public class Sql2JSONCommandLine {
 
